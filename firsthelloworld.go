abc434cbf567c16f729ec664094d60aa50d91618
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-11-01 22:59:44 +0100

    doc comment

--- firsthelloworld.go
+++ firsthelloworld.go
@@ -1,10 +1,13 @@
+/*
+Required package doc comment.
+*/
 package main
 
 import (
-    "fmt"
-    "github.com/gabr/firsthelloworld/stringtools"
+	"fmt"
+	"github.com/gabr/firsthelloworld/stringtools"
 )
 
 func main() {
-    fmt.Printf(stringtools.Reverse("Hello World!"))
+	fmt.Printf(stringtools.Reverse("Hello World!"))
 }
